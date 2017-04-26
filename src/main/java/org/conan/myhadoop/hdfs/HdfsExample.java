d5f0ed0ad6d9a6dbccc19470d8c6f7c26f4a6bf2
zhangzhibo <columbia@163.com>
2017-04-26 17:12:34 +0800

    2017.04.26 17:12

---
+++ src/main/java/org/conan/myhadoop/hdfs/HdfsExample.java
@@ -0,0 +1,59 @@
+package org.conan.myhadoop.hdfs;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * Created by zhangzhibo on 17-4-26.
+ */
+public class HdfsExample {
+    public static void testMkdirPath(String path) throws Exception {
+        FileSystem fs = null;
+        try {
+            System.out.println("Create " + path + " on hdfs ...");
+            Configuration con = new Configuration();
+            Path myPath = new Path(path);
+            fs = myPath.getFileSystem(con);
+            fs.mkdirs(myPath);
+            System.out.println("Create " + path + " on hdfs successfully ! ");
+        }catch(Exception e)
+        {
+            System.out.println("Exception: " + e);
+        }finally{
+            if (fs != null) {
+                fs.close();
+            }
+        }
+    }
+    public static void testDeletePath(String path) throws Exception{
+        FileSystem fs = null;
+        try{
+            System.out.println("Delete " + path + " on hdfs ...");
+            Configuration con = new Configuration();
+            Path myPath = new Path(path);
+            fs = myPath.getFileSystem(con);
+            fs.delete(myPath,true);
+            System.out.println("Delete " + path + " on hdfs successfully ! ");
+        }catch(Exception e)
+        {
+            System.out.println("Exception: " + e);
+        }finally{
+            if (fs != null) {
+                fs.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String path = "hdfs:master:50070/test/mkdir-test";
+            testDeletePath(path);
+        }catch (Exception e){
+            System.out.println("Exceptions : "+e);
+        }
+        System.out.println("TimeStamp : "+System.currentTimeMillis());
+
+    }
+}
