0e04aa616d6cc07906b53aec1931c7ac8bd4344f
zhangzhibo <columbia@163.com>
2017-05-25 21:12:15 +0800

    2017.05.25 21:12

---
+++ src/main/java/org/conan/myhadoop/HadoopDefinitiveGuide/chap04/AvroGenericMaxTemperature.java
@@ -0,0 +1,207 @@
+package org.conan.myhadoop.HadoopDefinitiveGuide.chap04;
+
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.mapred.AvroCollector;
+import org.apache.avro.mapred.AvroJob;
+import org.apache.avro.mapred.AvroMapper;
+import org.apache.avro.mapred.AvroReducer;
+import org.apache.avro.mapred.AvroUtf8InputFormat;
+import org.apache.avro.mapred.Pair;
+import org.apache.avro.util.Utf8;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * Created by zhangzhibo on 17-5-24.
+ */
+class NcdcRecordParser {
+    private static final int MISSING_TEMPERATURE = 9999;
+
+    private static final DateFormat DATE_FORMAT =
+            new SimpleDateFormat("yyyyMMddHHmm");
+
+    private String stationId;
+    private String observationDateString;
+    private String year;
+    private String airTemperatureString;
+    private int airTemperature;
+    private boolean airTemperatureMalformed;
+    private String quality;
+
+    public void parse(String record) {
+        stationId = record.substring(4, 10) + "-" + record.substring(10, 15);
+        observationDateString = record.substring(15, 27);
+        year = record.substring(15, 19);
+        airTemperatureMalformed = false;
+        // Remove leading plus sign as parseInt doesn't like them (pre-Java 7)
+        if (record.charAt(87) == '+') {
+            airTemperatureString = record.substring(88, 92);
+            airTemperature = Integer.parseInt(airTemperatureString);
+        } else if (record.charAt(87) == '-') {
+            airTemperatureString = record.substring(87, 92);
+            airTemperature = Integer.parseInt(airTemperatureString);
+        } else {
+            airTemperatureMalformed = true;
+        }
+        airTemperature = Integer.parseInt(airTemperatureString);
+        quality = record.substring(92, 93);
+    }
+
+    public void parse(Text record) {
+        parse(record.toString());
+    }
+
+    public boolean isValidTemperature() {
+        return !airTemperatureMalformed && airTemperature != MISSING_TEMPERATURE
+                && quality.matches("[01459]");
+    }
+
+    public boolean isMalformedTemperature() {
+        return airTemperatureMalformed;
+    }
+
+    public boolean isMissingTemperature() {
+        return airTemperature == MISSING_TEMPERATURE;
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public Date getObservationDate() {
+        try {
+            System.out.println(observationDateString);
+            return DATE_FORMAT.parse(observationDateString);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getYearInt() {
+        return Integer.parseInt(year);
+    }
+
+    public int getAirTemperature() {
+        return airTemperature;
+    }
+
+    public String getAirTemperatureString() {
+        return airTemperatureString;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+
+}
+
+public class AvroGenericMaxTemperature extends Configured implements Tool {
+
+    private static final Schema SCHEMA = new Schema.Parser().parse(
+            "{" +
+                    "\"type\": \"record\"," +
+                    " \"name\": \"WeatherRecord\"," +
+                    " \"doc\" : \"A weather reading\"," +
+                    " \"fields\": [" +
+                    "     {\"name\": \"year\", \"type\": \"int\"}," +
+                    "     {\"name\": \"temperature\", \"type\": \"int\"}," +
+                    "     {\"name\": \"stationID\", \"type\": \"string\"}" +
+                    " ]" +
+                    "}"
+    );
+
+    public static class MaxTemperatureMapper extends AvroMapper<Utf8, Pair<Integer, GenericRecord>> {
+        private NcdcRecordParser parser = new NcdcRecordParser();
+        private GenericRecord record = new GenericData.Record(SCHEMA);
+
+        @Override
+        public void map(Utf8 line, AvroCollector<Pair<Integer, GenericRecord>> collector, Reporter reporter) throws IOException {
+            parser.parse(line.toString());
+            if (parser.isValidTemperature()) {
+                record.put("year", parser.getYearInt());
+                record.put("temperature", parser.getAirTemperature());
+                record.put("stationId", parser.getStationId());
+                collector.collect(new Pair<Integer, GenericRecord>(parser.getYearInt(), record));
+            }
+        }
+    }
+
+    public static class MaxTemperatureReducer extends AvroReducer<Integer, GenericRecord, GenericRecord> {
+        @Override
+        public void reduce(Integer key, Iterable<GenericRecord> values, AvroCollector<GenericRecord> collector, Reporter reporter) throws IOException {
+            GenericRecord max = null;
+            for (GenericRecord value :
+                    values) {
+                if (max == null || (Integer) value.get("temperature") > (Integer) max.get("temperature")) {
+                    max = newWeatherRecord(value);
+                }
+            }
+            collector.collect(max);
+        }
+
+        private GenericRecord newWeatherRecord(GenericRecord value) {
+            GenericRecord record = new GenericData.Record(SCHEMA);
+            record.put("year", value.get("year"));
+            record.put("temperature", value.get("temperature"));
+            record.put("stationId", value.get("stationId"));
+            return record;
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage : %s [generic options] <input> <output>\n", getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+        JobConf conf = new JobConf(getConf(), getClass());
+        conf.setJobName("Max temperature");
+
+        FileInputFormat.addInputPath(conf, new Path(args[0]));
+//            FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        AvroJob.setInputSchema(conf, Schema.create(Schema.Type.STRING));
+        AvroJob.setMapOutputSchema(conf, SCHEMA);
+        conf.setInputFormat(AvroUtf8InputFormat.class);
+
+        AvroJob.setMapperClass(conf, MaxTemperatureMapper.class);
+        AvroJob.setReducerClass(conf, MaxTemperatureReducer.class);
+
+        JobClient.runJob(conf);
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new AvroGenericMaxTemperature(), args);
+        System.exit(exitCode);
+
+    }
+
+}
+
+
