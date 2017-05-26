54769feca17310b56f4968fb35a7ff2c549116df
zhangzhibo <columbia@163.com>
2017-05-26 21:29:38 +0800

    2017.05.25 21:12

--- src/main/java/org/conan/myhadoop/HadoopDefinitiveGuide/chap04/AvroGenericMaxTemperature.java
+++ src/main/java/org/conan/myhadoop/HadoopDefinitiveGuide/chap04/AvroGenericMaxTemperature.java
@@ -117,147 +117,51 @@ class NcdcRecordParser {
 
 
 }
-//
-//public class AvroGenericMaxTemperature extends Configured implements Tool {
-//
-//    private static final Schema SCHEMA = new Schema.Parser().parse(
-//            "{" +
-//                    "\"type\": \"record\"," +
-//                    " \"name\": \"WeatherRecord\"," +
-//                    " \"doc\" : \"A weather reading\"," +
-//                    " \"fields\": [" +
-//                    "     {\"name\": \"year\", \"type\": \"int\"}," +
-//                    "     {\"name\": \"temperature\", \"type\": \"int\"}," +
-//                    "     {\"name\": \"stationID\", \"type\": \"string\"}" +
-//                    " ]" +
-//                    "}"
-//    );
-//
-//    public static class MaxTemperatureMapper extends AvroMapper<Utf8, Pair<Integer, GenericRecord>> {
-//        private NcdcRecordParser parser = new NcdcRecordParser();
-//        private GenericRecord record = new GenericData.Record(SCHEMA);
-//
-//        @Override
-//        public void map(Utf8 line, AvroCollector<Pair<Integer, GenericRecord>> collector, Reporter reporter) throws IOException {
-//            parser.parse(line.toString());
-//            if (parser.isValidTemperature()) {
-//                record.put("year", parser.getYearInt());
-//                record.put("temperature", parser.getAirTemperature());
-//                record.put("stationId", parser.getStationId());
-//                collector.collect(new Pair<Integer, GenericRecord>(parser.getYearInt(), record));
-//            }
-//        }
-//    }
-//
-//    public static class MaxTemperatureReducer extends AvroReducer<Integer, GenericRecord, GenericRecord> {
-//        @Override
-//        public void reduce(Integer key, Iterable<GenericRecord> values, AvroCollector<GenericRecord> collector, Reporter reporter) throws IOException {
-//            GenericRecord max = null;
-//            for (GenericRecord value :
-//                    values) {
-//                if (max == null || (Integer) value.get("temperature") > (Integer) max.get("temperature")) {
-//                    max = newWeatherRecord(value);
-//                }
-//            }
-//            collector.collect(max);
-//        }
-//
-//        private GenericRecord newWeatherRecord(GenericRecord value) {
-//            GenericRecord record = new GenericData.Record(SCHEMA);
-//            record.put("year", value.get("year"));
-//            record.put("temperature", value.get("temperature"));
-//            record.put("stationId", value.get("stationId"));
-//            return record;
-//        }
-//    }
-//
-//    @Override
-//    public int run(String[] args) throws Exception {
-//        if (args.length != 2) {
-//            System.err.printf("Usage : %s [generic options] <input> <output>\n", getClass().getSimpleName());
-//            ToolRunner.printGenericCommandUsage(System.err);
-//            return -1;
-//        }
-//        JobConf conf = new JobConf(getConf(), getClass());
-//        conf.setJobName("Max temperature");
-//
-//        FileInputFormat.addInputPath(conf, new Path(args[0]));
-////            FileOutputFormat.setOutputPath(conf,new Path(args[1]));
-//        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-//
-//        AvroJob.setInputSchema(conf, Schema.create(Schema.Type.STRING));
-//        AvroJob.setMapOutputSchema(conf,Pair.getPairSchema(Schema.create(Schema.Type.INT), SCHEMA));
-//        AvroJob.setMapOutputSchema(conf, SCHEMA);
-//
-//        conf.setInputFormat(AvroUtf8InputFormat.class);
-//
-//        AvroJob.setMapperClass(conf, MaxTemperatureMapper.class);
-//        AvroJob.setReducerClass(conf, MaxTemperatureReducer.class);
-//
-//        JobClient.runJob(conf);
-//
-//        return 0;
-//    }
-//
-//    public static void main(String[] args) throws Exception {
-//        int exitCode = ToolRunner.run(new AvroGenericMaxTemperature(), args);
-//        System.exit(exitCode);
-//
-//    }
-//
-//}
-
 
 public class AvroGenericMaxTemperature extends Configured implements Tool {
 
     private static final Schema SCHEMA = new Schema.Parser().parse(
             "{" +
-                    "  \"type\": \"record\"," +
-                    "  \"name\": \"WeatherRecord\"," +
-                    "  \"doc\": \"A weather reading.\"," +
-                    "  \"fields\": [" +
-                    "    {\"name\": \"year\", \"type\": \"int\"}," +
-                    "    {\"name\": \"temperature\", \"type\": \"int\"}," +
-                    "    {\"name\": \"stationId\", \"type\": \"string\"}" +
-                    "  ]" +
+                    "\"type\": \"record\"," +
+                    " \"name\": \"WeatherRecord\"," +
+                    " \"doc\" : \"A weather reading\"," +
+                    " \"fields\": [" +
+                    "     {\"name\": \"year\", \"type\": \"int\"}," +
+                    "     {\"name\": \"temperature\", \"type\": \"int\"}," +
+                    "     {\"name\": \"stationID\", \"type\": \"string\"}" +
+                    " ]" +
                     "}"
     );
 
-    public static class MaxTemperatureMapper
-            extends AvroMapper<Utf8, Pair<Integer, GenericRecord>> {
+    public static class MaxTemperatureMapper extends AvroMapper<Utf8, Pair<Integer, GenericRecord>> {
         private NcdcRecordParser parser = new NcdcRecordParser();
         private GenericRecord record = new GenericData.Record(SCHEMA);
+
         @Override
-        public void map(Utf8 line,
-                        AvroCollector<Pair<Integer, GenericRecord>> collector,
-                        Reporter reporter) throws IOException {
+        public void map(Utf8 line, AvroCollector<Pair<Integer, GenericRecord>> collector, Reporter reporter) throws IOException {
             parser.parse(line.toString());
             if (parser.isValidTemperature()) {
                 record.put("year", parser.getYearInt());
                 record.put("temperature", parser.getAirTemperature());
                 record.put("stationId", parser.getStationId());
-                collector.collect(
-                        new Pair<Integer, GenericRecord>(parser.getYearInt(), record));
+                collector.collect(new Pair<Integer, GenericRecord>(parser.getYearInt(), record));
             }
         }
     }
 
-    public static class MaxTemperatureReducer
-            extends AvroReducer<Integer, GenericRecord, GenericRecord> {
-
+    public static class MaxTemperatureReducer extends AvroReducer<Integer, GenericRecord, GenericRecord> {
         @Override
-        public void reduce(Integer key, Iterable<GenericRecord> values,
-                           AvroCollector<GenericRecord> collector, Reporter reporter)
-                throws IOException {
+        public void reduce(Integer key, Iterable<GenericRecord> values, AvroCollector<GenericRecord> collector, Reporter reporter) throws IOException {
             GenericRecord max = null;
-            for (GenericRecord value : values) {
-                if (max == null ||
-                        (Integer) value.get("temperature") > (Integer) max.get("temperature")) {
+            for (GenericRecord value :
+                    values) {
+                if (max == null || (Integer) value.get("temperature") > (Integer) max.get("temperature")) {
                     max = newWeatherRecord(value);
                 }
             }
             collector.collect(max);
         }
+
         private GenericRecord newWeatherRecord(GenericRecord value) {
             GenericRecord record = new GenericData.Record(SCHEMA);
             record.put("year", value.get("year"));
@@ -270,22 +174,20 @@ public class AvroGenericMaxTemperature extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.printf("Usage: %s [generic options] <input> <output>\n",
-                    getClass().getSimpleName());
+            System.err.printf("Usage : %s [generic options] <input> <output>\n", getClass().getSimpleName());
             ToolRunner.printGenericCommandUsage(System.err);
             return -1;
         }
-
         JobConf conf = new JobConf(getConf(), getClass());
         conf.setJobName("Max temperature");
 
         FileInputFormat.addInputPath(conf, new Path(args[0]));
+//            FileOutputFormat.setOutputPath(conf,new Path(args[1]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
         AvroJob.setInputSchema(conf, Schema.create(Schema.Type.STRING));
-        AvroJob.setMapOutputSchema(conf,
-                Pair.getPairSchema(Schema.create(Schema.Type.INT), SCHEMA));
-        AvroJob.setOutputSchema(conf, SCHEMA);
+        AvroJob.setMapOutputSchema(conf, Pair.getPairSchema(Schema.create(Schema.Type.INT), SCHEMA));
+        AvroJob.setMapOutputSchema(conf, SCHEMA);
 
         conf.setInputFormat(AvroUtf8InputFormat.class);
 
@@ -293,13 +195,17 @@ public class AvroGenericMaxTemperature extends Configured implements Tool {
         AvroJob.setReducerClass(conf, MaxTemperatureReducer.class);
 
         JobClient.runJob(conf);
+
         return 0;
     }
 
     public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(new AvroGenericMaxTemperature(), args);
         System.exit(exitCode);
+
     }
+
 }
+
 
 
